a301178948ff390fcb99ee41ddead6d378e9f256
Chris Williams <chris@innotrade.io>
2018-07-03 10:11:55 -0600

    smart contracts bug fix uint256 vs unit256 spelling

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,20 +1,24 @@
-pragma solidity 0.4.20;
+pragma solidity ^0.4.20;
+
 contract Casino {
     address public owner;
-    unit256 public minimumBet;
-    unit256 public totalBet;
-    unit256 public numberOfBets;
-    unit256 public maxAmountOfBets = 100;
+    uint256 public minimumBet;
+    uint256 public totalBet;
+    uint256 public numberOfBets;
+    uint256 public maxAmountOfBets = 100;
     address[] public players;
 
     struct Player { 
-        unit256 amountBet;
-        unit256 numberSelected;
+        uint256 amountBet;
+        uint256 numberSelected;
     }
 
-    // The address of the palyer and => the user info mapping (address => Player) public playerInfo;
+    // The address of the player and => the user info
+    mapping (address => Player) public playerInfo;
+
+    function() public payable {}
     
-    function Casino(unit256 _minimumBet) public { 
+    constructor(uint256 _minimumBet) public { 
         owner = msg.sender;
         if(_minimumBet != 0 ) minimumBet = _minimumBet;
     }
@@ -24,8 +28,8 @@ contract Casino {
         if (msg.sender == owner) selfdestruct(owner);
     }
 
-    function checkplayerExisits(address player) public constant returns(bool){ 
-        for(unit256 i = 0; i < plalyers.length; i++){
+    function checkplayerExists(address player) public constant returns(bool){ 
+        for(uint256 i = 0; i < players.length; i++){
             if(players[i] == player) return true;
         }
         return false;
@@ -33,13 +37,13 @@ contract Casino {
 
 
     // To bet for a number between 1 and 10 both inclusive 
-    function bet(unit256 numberSelected) public payable { 
-        require(!checkplayerExisits(msg.sender));
-        require(numberSelected >= 1 && numberSelected <- 10);
+    function bet(uint256 numberSelected) public payable { 
+        require(!checkplayerExists(msg.sender));
+        require(numberSelected >= 1 && numberSelected <= 10);
         require(msg.value >= minimumBet);
 
-        playersInfo[msg.sender].amountBet = msg.value;
-        palyerInfo[msg.sender].numberSelected = numberSelected;
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].numberSelected = numberSelected;
         numberOfBets++;
         players.push(msg.sender);
         totalBet += msg.value;
@@ -47,17 +51,17 @@ contract Casino {
 
     // Generates a number between 1 and 10 that will be the winner
     function generateNumberWinner() public {
-        unit256 numberGenerated = block.number % 10 + 1; // This isn't secure 
+        uint256 numberGenerated = block.number % 10 + 1; // This isn't secure 
         distributePrizes(numberGenerated);
     }
 
     // Sends the corresponding ether to each winner depending on the toal bets 
     
-    function distributePrizes(unit256 numberWinner) public { 
+    function distributePrizes(uint256 numberWinner) public { 
         address[100] memory winners; // We have to create a temporary in memory array with fixed size
-        uinit256 count = 0; // This is the count for the array of winners
+        uint256 count = 0; // This is the count for the array of winners
         
-        for(uinit256 i = 0; i < players.length; i++){
+        for(uint256 i = 0; i < players.length; i++){
             address playerAddress = players[i];
             if(playerInfo[playerAddress].numberSelected == numberWinner){
                 winners[count] = playerAddress;
@@ -68,9 +72,9 @@ contract Casino {
 
         players.length = 0; // Delete all the players array
 
-        uinit256 winnerEtherAmount = totalBet / winners.length; //How much each winner gets
+        uint256 winnerEtherAmount = totalBet / winners.length; //How much each winner gets
 
-        for(uinit256 j = 0; j < count; j++){
+        for(uint256 j = 0; j < count; j++){
             if(winners [j] != address(0)) // Check that the address in this fixed array is not empty
             winners [j].transfer(winnerEtherAmount);
         }
