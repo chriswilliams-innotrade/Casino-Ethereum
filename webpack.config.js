f7a4c5d57dd4c725baa4893a2bbd08a90df71cfb
Chris Williams <chris@innotrade.io>
2018-07-04 15:46:24 -0600

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,16 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'build.js' // The final file will be created in dist/build.js
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.css$/, // To load the css in react 
-            use: ['style-loader', 'css-laoder'],
+            use: ['style-loader', 'css-loader'],
             include: /src/
         }, {
             test: /\.jsx?$/, // To load the js an jsx files
@@ -22,7 +23,8 @@ module.exports = {
             }
         }, {
             test: /\.json$/, // To laod the json files
-            loader: 'json-loader'
+            loader: 'json-loader',
+            exclude: /node_modules/
         }]
     }
 }
